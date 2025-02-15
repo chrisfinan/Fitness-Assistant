d926e39bb2e9c80242592b0f100349a74a06daf6
Michael Wood <mw746845@sju.edu>
2025-02-15 14:43:11 -0500

    Clean up

--- app/main.py
+++ app/main.py
@@ -11,12 +11,3 @@ origins = [
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(exercises.router, prefix="/exercises", tags=["Exercises"])
 
-# Include routers so their APIs are in FastAPI
-#app.include_router(users.router, prefix="/users", tags=["Users"])
-#/ app.include_router(trips.router, prefix="/trips", tags=["Trips"])
-#app.include_router(
-#    campus_locations.router, prefix="/campus_locations", tags=["Campus Locations"]
-#)
-#app.include_router(vehicles.router, prefix="/vehicles", tags=["Vehicles"])
-#app.include_router(shuttles.router, prefix="/shuttles", tags=["Shuttles"])
-#app.include_router(drivers.router, prefix="/drivers", tags=["Drivers"])
