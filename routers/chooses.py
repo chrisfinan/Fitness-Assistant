04c59f0661ee8d3c74ba9056cbbe40833b1b4557
chris <cf751653@sju.edu>
2025-03-22 14:44:30 -0400

    add join api for choose and exercises

--- routers/chooses.py
+++ routers/chooses.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, HTTPException, Depends, APIRouter
-from app.models import Base, Choose
+from app.models import Base, Choose, Exercise
 from schemas.choose import ChooseResponse, ChooseUpdate
 from app.db import get_db
 
@@ -35,3 +35,12 @@ async def update_user(
         return ChooseResponse.from_orm(choose_to_update)
     else:
         raise HTTPException(status_code=404, detail=f"User with ID {uid} not found")
+
+@router.get("/exercises/{uid}")
+async def get_exercises_for_user(uid: int, db: Session = Depends(get_db)):
+    exercises = db.query(Exercise).join(Choose, Choose.eid == Exercise.eid).filter(Choose.uid == uid).all()
+
+    if not exercises:
+        raise HTTPException(status_code=404, detail=f"No exercises found for user {uid}")
+
+    return exercises
